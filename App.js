ef4a0844ac8c3131c8456070ffaa5a735e6de9c9
jeongminDev <bjm4256@gmail.com>
2025-04-30 13:32:34 +0900

    [FEAT] 날씨 앱 학습습

--- App.js
+++ App.js
@@ -1,11 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from 'react-native';
+import Fontisto from '@expo/vector-icons/Fontisto';
+// import dayjs from 'dayjs';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+// const SCREEN_WIDTH = Dimensions.get('window').width; // 위랑 똑같은 내용
+
+const API_KEY = '16b2a493e6745e3af510bbb6e2c6e1c9';
+// const API_KEY2 =
+//   'n%2B5UEBjFeHWmjVTyVmr9STPjpcqh%2F27NANSHDxurDQ6E3pI1shpexnjOdfod%2Fu6Z5H6%2B8TCKLp%2FLsaE4gtvvLw%3D%3D'; // 기상청청
+
+const icons = {
+  Clouds: 'cloudy',
+  Clear: 'day-sunny',
+  Atmosphere: 'cloudy-gusts',
+  Snow: 'snow',
+  Rain: 'rains',
+  Drizzle: 'rain',
+  Thunderstorm: 'lightning',
+};
 
 export default function App() {
+  // const formattedDate = dayjs().format('YYYYMMDD');
+
+  const [city, setCity] = useState('Loading...');
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.list);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: 'center' }}>
+            <ActivityIndicator color="black" size="large" />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.secDescription}>{day.dt_txt}</Text>
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  width: '100%',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.main.temp).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={50}
+                  color="white"
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.secDescription}>
+                {day.weather[0].description}
+              </Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +110,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#AAD7F6',
+  },
+  city: {
+    flex: 1.2,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    color: '#fff',
+    fontSize: 48,
+    fontWeight: 400,
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: 'flex-start',
+    paddingHorizontal: 20,
+  },
+  temp: {
+    marginTop: 60,
+    fontSize: 100,
+    color: '#fff',
+  },
+  description: {
+    marginTop: -10,
+    fontSize: 40,
+    fontWeight: 600,
+    color: '#fff',
+  },
+  secDescription: {
+    fontSize: 20,
+    color: '#eee',
   },
 });
